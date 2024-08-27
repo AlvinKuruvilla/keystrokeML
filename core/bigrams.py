5cb140e44cf052cb531fa7ce42291393a4eb3f4c
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2024-08-26 22:27:15 -0400

    Use frequencies from norvig daatset to find the n most common bigrams

--- core/bigrams.py
+++ core/bigrams.py
@@ -1,5 +1,7 @@
 import os
+import pandas as pd
 from collections import Counter
+from functools import cache
 
 
 # http://norvig.com/mayzner.html
@@ -111,6 +113,41 @@ def sorted_bigrams_frequency(words):
     """Sort bigrams by frequency from highest to lowest."""
     bigram_counts = generate_bigrams(words)
     return sorted(bigram_counts.items(), key=lambda x: x[1], reverse=True)
+
+
+def generate_unique_bigrams(word):
+    bigrams = set()  # Using a set to store unique bigrams
+    word = str(word)
+    for i in range(len(word) - 1):
+        bigram = str(word[i : i + 2])
+        bigrams.add(bigram)
+    return bigrams
+
+
+@cache
+def read_norvig_words(n=15):
+    df = pd.read_csv("peter_norvig_words.txt", sep="\s+", header=None)
+    # Extract the first column
+    bigrams = []
+    bigram_pairs = {}
+    words = list(df[0])
+    frequencies = list(df[1])
+    for word in words:
+        word = str(word)
+        print(word)
+        bigrams.extend(generate_unique_bigrams(word))
+    unique_bigrams = set(bigrams)
+    for bigram in unique_bigrams:
+        for idx, word in enumerate(words):
+            if bigram in str(word):
+                if bigram in bigram_pairs:
+                    bigram_pairs[bigram] += frequencies[idx]
+                else:
+                    bigram_pairs[bigram] = 0
+    sorted_dict = dict(
+        sorted(bigram_pairs.items(), key=lambda item: item[1], reverse=True)
+    )
+    return list(sorted_dict.keys())[0:n]
 
 
 if __name__ == "__main__":
